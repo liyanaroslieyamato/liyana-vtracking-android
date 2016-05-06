693c96758a6b1409da5bcc39d7b449473d0ad4c4
liyanaroslieyamato <liyana@yamatoasia.com>
2016-05-06 22:02:03 +0800

    Change to live and change token

--- android/app/src/main/java/yamato/vtracking/config/AppConfig.java
+++ android/app/src/main/java/yamato/vtracking/config/AppConfig.java
@@ -6,6 +6,6 @@ package yamato.vtracking.config;
 public class AppConfig {
 
     // Server user register url
-    public static final String ENDPOINT = "http://staging.yamatotracking.com";
-    public static final String TOKEN = "Token token=\"HAPPY\"";
+    public static final String ENDPOINT = "http://yamatotracking.com";
+    public static final String TOKEN = "Token token=\"t5ygg5RxbuPYhCtC6LNj28ciWUDcNt7G0hVfouIfXAzq46FUYXEtmnzyGU6xTgowQaFyhxVqUOQRm808htL8AvWNbhNIlRfkI8ePhwNbp4qq9FMhICsmvTWYdzmL3mkC\"";
 }
